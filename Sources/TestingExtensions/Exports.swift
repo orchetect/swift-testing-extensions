4987436a8599ce3dd8e782e8a4e905d771ce4987
Steffan Andrews <stef@orchetect.com>
2024-10-25 04:03:45 -0700

    `TestResource`: Added export for Foundation to avoid compiler error when calling `data()` or `url()`

---
+++ Sources/TestingExtensions/Exports.swift
@@ -0,0 +1,8 @@
+//
+//  Exports.swift
+//  swift-testing-extensions • https://github.com/orchetect/swift-testing-extensions
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+// Ensures that calls to TestResource succeed when expanding the `#moduleBundle` macro
+@_exported import Foundation
