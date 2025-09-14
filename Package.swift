5a7f87724a153620635a02ef87879f8388770a72
Steffan Andrews <stef@orchetect.com>
2025-09-13 17:27:30 -0700

    swift-syntax dependency now uses latest major version where possible, maintaining backwards compatibiliity

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "99999999.999.999")
     ],
     targets: [
         .target(
